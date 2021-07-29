6cd7d71e550b22a9461acfcea3520a70da3c327f
aman706 <76611203+aman706@users.noreply.github.com>
2021-07-29 18:10:32 +0530

    Add files via upload

--- YoneRobot/lyrics.py
+++ YoneRobot/lyrics.py
@@ -1,11 +1,9 @@
 from aiohttp import ClientSession
-from Python_ARQ import ARQ
-
-from from aiohttp import ClientSession
-from Python_ARQ import ARQ
-
-from Yone import *
-from Yone.events import register
+import html 
+import re 
+from PYTHON_ARQ import ARQ
+from YoneRobot import *
+from YoneRobot.events import register
 
 #value
 ARQ_API_KEY = "QCRJIN-MJOBNZ-SPYKTN-NFZKZZ-ARQ"
@@ -14,40 +12,10 @@ ARQ_API_URL = "https://thearq.tech/"
 aiohttpsession = ClientSession()
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
-from Yone import pbot as app
-from Yone.pyrogramee.errors import capture_err
-from Yone.pyrogramee.json_prettify import json_prettify
-from Yone.pyrogramee.fetch import fetch
-from pyrogram import filters
-
-@app.on_message(filters.command("covid") & ~filters.edited)
-@capture_err
-async def lyrics_func(_, message):
-    if len(message.command) < 2:
-        await message.reply_text("**Sike That's The Wrong Command Usage!** \nUse `/lyrics` (song name)")
-        return
-    m = await message.reply_text("**Searching For Song Lyrics**")
-    query = message.text.strip().split(None, 1)[1]
-    song = await arq.lyrics(query)
-    lyrics = song.result
-    if len(lyrics) < 4095:
-        await m.edit(f"__{lyrics}__")
-        return
-    lyrics = await paste(lyrics)
-    await m.edit(f"**Oops! Lyrics Too Long To Send!** \n**Your Song Lyrics: [Click Here]({lyrics})**") import *
-from Yone.events import register
-
-#value
-ARQ_API_KEY = "QCRJIN-MJOBNZ-SPYKTN-NFZKZZ-ARQ"
-ARQ_API_URL = "https://thearq.tech/"
-
-aiohttpsession = ClientSession()
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
-
-from Yone import pbot as app
-from Yone.pyrogramee.errors import capture_err
-from Yone.pyrogramee.json_prettify import json_prettify
-from Yone.pyrogramee.fetch import fetch
+from YoneRobot import pbot as app
+from YoneRobot.pyrogramee.errors import capture_err
+from YoneRobot.pyrogramee.json_prettify import json_prettify
+from YoneRobot.pyrogramee.fetch import fetch
 from pyrogram import filters
 
 @app.on_message(filters.command("covid") & ~filters.edited)
