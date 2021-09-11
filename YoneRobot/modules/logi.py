45d63fa541d617776165a5352352bb1036632996
aman706 <76611203+aman706@users.noreply.github.com>
2021-09-11 16:09:12 +0530

    Update logi.py

--- YoneRobot/modules/logi.py
+++ YoneRobot/modules/logi.py
@@ -7,7 +7,7 @@ from telethon.tl.types import InputMessagesFilterPhotos
 
 
 
-@register(pattern="^/logol ?(.*)")
+@register(pattern="^/logol")
 async def logo_gen(event):
     xx = await eor(event, get_string("com_1"))
     name = event.pattern_match.group(1)
